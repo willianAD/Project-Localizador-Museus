675d7626f0873dc9fff2ce30a25f82676d24aa17
willianAD <willian.afonso013@gmail.com>
2023-07-19 18:14:36 -0300

    feat: Method getClosestMuseum - Requisito 4

---
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -0,0 +1,8 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * Class MuseumNotFoundException.
+ */
+public class MuseumNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+}
